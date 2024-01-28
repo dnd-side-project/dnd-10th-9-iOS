793eae4cf97d43be50d8e767063921ca7ac37739
yubin <yubin412@naver.com>
2024-01-28 23:04:10 +0900

    docs: 프로젝트 생성
    초기 개발 환경 세팅

---
+++ dnd-10th-9/dnd_10th_9App.swift
@@ -0,0 +1,17 @@
+//
+//  dnd_10th_9App.swift
+//  dnd-10th-9
+//
+//  Created by yubin on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct dnd_10th_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
