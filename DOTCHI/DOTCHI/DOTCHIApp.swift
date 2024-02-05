9aa1c8c614ab62fbff41a35cb07dedafda99e3d2
Jungbin Jung <lollin0@naver.com>
2024-02-05 19:46:29 +0900

    add: DOTCHI 프로젝트 생성 #6

---
+++ DOTCHI/DOTCHI/DOTCHIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DOTCHIApp.swift
+//  DOTCHI
+//
+//  Created by Jungbin on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct DOTCHIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
