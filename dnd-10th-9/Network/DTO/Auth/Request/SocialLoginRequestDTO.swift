ea000c8aa87bc14c489673eeb22c0cd2b40f2bb1
Jungbin Jung <lollin0@naver.com>
2024-02-05 14:53:54 +0900

    chore: request/response DTO 예시 코드 추가 #6

---
+++ dnd-10th-9/Network/DTO/Auth/Request/SocialLoginRequestDTO.swift
@@ -0,0 +1,20 @@
+//
+//  SocialLoginRequestDTO.swift
+//  dnd-10th-9
+//
+//  Created by Jungbin on 2/5/24.
+//
+
+import Foundation
+
+struct SocialLoginRequestDTO: Encodable {
+//    let token: String
+//    let socialType: String
+//    let deviceToken: String
+//    
+//    init(token: String, socialType: SocialType, deviceToken: String) {
+//        self.token = token
+//        self.socialType = socialType.rawValue
+//        self.deviceToken = deviceToken
+//    }
+}
