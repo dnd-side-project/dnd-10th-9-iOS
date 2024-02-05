3025111361159dc4be85b0dbd75dd5b55a7858d0
Jungbin Jung <lollin0@naver.com>
2024-02-05 13:34:28 +0900

    add: Environment enum 추가 #6

---
+++ dnd-10th-9/Supprots/Environment.swift
@@ -0,0 +1,42 @@
+//
+//  Environment.swift
+//  dnd-10th-9
+//
+//  Created by Jungbin on 2/5/24.
+//
+
+import Foundation
+
+enum Environment: String {
+    case debug = "debug"
+    case qa = "qa"
+    case release = "release"
+    
+    enum Keys {
+        enum Plist {
+            static let baseURL = "BASE_URL"
+        }
+    }
+    
+    static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else { fatalError() }
+        return dict
+    }()
+    
+    static let BASE_URL: String = {
+        guard let string = Environment.infoDictionary[Keys.Plist.baseURL] as? String else {
+            fatalError("Base URL not set in plist for this environment")
+        }
+        return string
+    }()
+}
+
+func env() -> Environment {
+    #if DEBUG
+    return .debug
+    #elseif QA
+    return .qa
+    #elseif RELEASE
+    return .release
+    #endif
+}
