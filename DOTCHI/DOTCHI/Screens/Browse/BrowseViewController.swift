87a44b65b964ea3f48c476b46b658cf9ab523065
Jungbin Jung <lollin0@naver.com>
2024-02-11 03:36:07 +0900

    remove: 임시 파일 삭제 #13

---
+++ DOTCHI/DOTCHI/Screens/Browse/BrowseViewController.swift
@@ -0,0 +1,40 @@
+//
+//  BrowseViewController.swift
+//  DOTCHI
+//
+//  Created by Jungbin on 2/11/24.
+//
+
+import UIKit
+import SnapKit
+
+final class BrowseViewController: BaseViewController {
+    
+    // MARK: UIComponents
+    
+    
+    
+    // MARK: Properties
+    
+    
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setLayout()
+    }
+    
+    // MARK: Methods
+    
+    
+}
+
+// MARK: - UI
+
+extension BrowseViewController {
+    private func setLayout() {
+        self.view.addSubviews([])
+    }
+}
--- DOTCHI/DOTCHI/Screens/Components/aaa.swift
+++
@@ -1,8 +0,0 @@
-//
-//  aaa.swift
-//  DOTCHI
-//
-//  Created by Jungbin on 2/5/24.
-//
-
-import Foundation
