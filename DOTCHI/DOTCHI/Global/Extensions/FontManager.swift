55f13d30afa30126267590f35346cc9e3195002a
yubin <yubin412@naver.com>
2024-02-11 20:28:18 +0900

    chore: FontManager extension 추가 #20
    - Pretendard-Bold.ttf 폰트 파일 추가
    - Info.plist 다시 세팅

---
+++ DOTCHI/DOTCHI/Global/Extensions/FontManager.swift
@@ -0,0 +1,22 @@
+//
+//  FontManager.swift
+//  DOTCHI
+//
+//  Created by yubin on 2/11/24.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    static let m12: Font = .custom("Pretendard-Medium", size: 12)
+    static let m14: Font = .custom("Pretendard-Medium", size: 14)
+    static let sb14: Font = .custom("Pretendard-SemiBold", size: 14)
+    static let sb16: Font = .custom("Pretendard-SemiBold", size: 16)
+    static let b14: Font = .custom("Pretendard-Bold", size: 14)
+    static let b16: Font = .custom("Pretendard-Bold", size: 16)
+    static let b32: Font = .custom("Pretendard-Bold", size: 32)
+    static let eb14: Font = .custom("Pretendard-ExtraBold", size: 14)
+    static let eb28: Font = .custom("Pretendard-ExtraBold", size: 28)
+    
+}
