e32d73ea3338d160649b04c09d1c6ebc47ea2f86
Jungbin Jung <lollin0@naver.com>
2024-02-14 00:59:19 +0900

    chore: UIFont+ 네이밍 수정 및 스타일 추가 #25

--- DOTCHI/DOTCHI/Global/Extensions/UIFont+.swift
+++ DOTCHI/DOTCHI/Global/Extensions/UIFont+.swift
@@ -8,39 +8,57 @@
 import UIKit.UIFont
 
 extension UIFont {
-    class var m12: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 12.0)!
-    }
-    
-    class var m14: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 14.0)!
-    }
-    
-    class var sb14: UIFont {
-        return UIFont(name: "Pretendard-SemiBold", size: 14.0)!
-    }
-    
-    class var sb16: UIFont {
-        return UIFont(name: "Pretendard-SemiBold", size: 16.0)!
-    }
-    
-    class var b14: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 14.0)!
-    }
-    
-    class var b16: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 16.0)!
-    }
-    
-    class var b32: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 32.0)!
-    }
-    
-    class var eb14: UIFont {
-        return UIFont(name: "Pretendard-ExtraBold", size: 14.0)!
-    }
-    
-    class var eb28: UIFont {
-        return UIFont(name: "Pretendard-ExtraBold", size: 28.0)!
-    }
+
+  class var bigTitle: UIFont {
+    return UIFont(name: "Pretendard-Bold", size: 32.0)!
+  }
+
+  class var dotchiName: UIFont {
+    return UIFont(name: "Pretendard-ExtraBold", size: 28.0)!
+  }
+
+  class var title: UIFont {
+    return UIFont(name: "Pretendard-Bold", size: 28.0)!
+  }
+
+  class var head: UIFont {
+    return UIFont(name: "Pretendard-Bold", size: 24.0)!
+  }
+
+  class var body: UIFont {
+    return UIFont(name: "Pretendard-Bold", size: 22.0)!
+  }
+
+  class var bigButton: UIFont {
+    return UIFont(name: "Pretendard-ExtraBold", size: 20.0)!
+  }
+
+  class var subTitle: UIFont {
+    return UIFont(name: "Pretendard-Bold", size: 18.0)!
+  }
+
+  class var dotchiName2: UIFont {
+    return UIFont(name: "Pretendard-ExtraBold", size: 16.0)!
+  }
+
+  class var head2: UIFont {
+    return UIFont(name: "Pretendard-Bold", size: 16.0)!
+  }
+
+  class var button: UIFont {
+    return UIFont(name: "Pretendard-ExtraBold", size: 14.0)!
+  }
+
+  class var subSbold: UIFont {
+    return UIFont(name: "Pretendard-SemiBold", size: 14.0)!
+  }
+
+  class var sub: UIFont {
+    return UIFont(name: "Pretendard-Medium", size: 14.0)!
+  }
+
+  class var sSub: UIFont {
+    return UIFont(name: "Pretendard-SemiBold", size: 12.0)!
+  }
+
 }
