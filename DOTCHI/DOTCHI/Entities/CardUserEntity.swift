3baefbd505cf32728fa2c8e6619960698bfaea84
Jungbin Jung <lollin0@naver.com>
2024-02-14 17:12:02 +0900

    chore: CardUserEntity 구현 #13

---
+++ DOTCHI/DOTCHI/Entities/CardUserEntity.swift
@@ -0,0 +1,14 @@
+//
+//  CardUserEntity.swift
+//  DOTCHI
+//
+//  Created by Jungbin on 2/14/24.
+//
+
+import Foundation
+
+struct CardUserEntity {
+    let userId: Int
+    let profileImageUrl: String
+    let username: String
+}
