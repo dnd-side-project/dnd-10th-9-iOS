dc7806d3f4b39be0f6669e86448ba2dbe56a933e
Jungbin Jung <lollin0@naver.com>
2024-02-14 10:57:49 +0900

    chore: 둘러보기 뷰 상단 UI 구현 #13

--- DOTCHI/DOTCHI/Screens/Browse/BrowseViewController.swift
+++ DOTCHI/DOTCHI/Screens/Browse/BrowseViewController.swift
@@ -10,9 +10,24 @@ import SnapKit
 
 final class BrowseViewController: BaseViewController {
     
+    private enum Text {
+        static let title = "따봉도치 둘러보기"
+    }
+    
     // MARK: UIComponents
     
-    private let navigationView: DotchiNavigationView = DotchiNavigationView(type: .back)
+    private let navigationView: DotchiNavigationUIView = DotchiNavigationUIView(type: .back)
+    
+    private let titleLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = Text.title
+        label.setStyle(.bigTitle, .white)
+        return label
+    }()
+    
+    private let latestButton: DotchiSortUIButton = DotchiSortUIButton(sortType: .latest)
+    
+    private let popularButton: DotchiSortUIButton = DotchiSortUIButton(sortType: .hot)
     
     // MARK: Properties
     
@@ -25,21 +40,61 @@ final class BrowseViewController: BaseViewController {
         
         self.setLayout()
         self.setBackButtonAction(self.navigationView.backButton)
+        self.setButtonToggle()
+        self.fetchData(isLatest: self.latestButton.isSelected)
     }
     
     // MARK: Methods
     
-    
+    private func setButtonToggle() {
+        self.latestButton.isSelected = true
+        
+        [self.latestButton, self.popularButton].forEach({ button in
+            button.setAction { [weak self] in
+                self?.latestButton.isSelected.toggle()
+                self?.popularButton.isSelected.toggle()
+                
+                self?.fetchData(isLatest: self?.latestButton.isSelected ?? true)
+            }
+        })
+    }
+}
+
+// MARK: - Network
+
+extension BrowseViewController {
+    private func fetchData(isLatest: Bool) {
+        // TODO: fetchData networking
+        debugPrint(#function, "isLatest \(isLatest)")
+    }
 }
 
 // MARK: - UI
 
 extension BrowseViewController {
     private func setLayout() {
-        self.view.addSubviews([navigationView])
+        self.view.addSubviews([navigationView, titleLabel, latestButton, popularButton])
         
         self.navigationView.snp.makeConstraints { make in
             make.top.horizontalEdges.equalTo(self.view.safeAreaLayoutGuide)
+        }
+        
+        self.titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.navigationView.snp.bottom).offset(24)
+            make.horizontalEdges.equalToSuperview().inset(28)
+            make.height.equalTo(32)
+        }
+        
+        self.latestButton.snp.makeConstraints { make in
+            make.top.equalTo(self.titleLabel.snp.bottom).offset(20)
+            make.leading.equalToSuperview().inset(28)
+            make.width.equalTo(42)
+            make.height.equalTo(22)
+        }
+        
+        self.popularButton.snp.makeConstraints { make in
+            make.top.width.height.equalTo(self.latestButton)
+            make.leading.equalTo(self.latestButton.snp.trailing).offset(9)
         }
     }
 }
