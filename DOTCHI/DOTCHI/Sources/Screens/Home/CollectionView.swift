c843f56fd9db6afc45113529800561936af94dad
yubin <yubin412@naver.com>
2024-02-15 17:04:04 +0900

    chore: Colorsets 네이밍 변경된 부분 적용 #36

--- DOTCHI/DOTCHI/Sources/Screens/Home/CollectionView.swift
+++ DOTCHI/DOTCHI/Sources/Screens/Home/CollectionView.swift
@@ -26,13 +26,13 @@ struct CollectionView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                Color.backgroundBlack.ignoresSafeArea()
+                Color.dotchiBlack.ignoresSafeArea()
                 
                 ScrollView(showsIndicators: false) {
                     VStack(alignment: .leading, spacing: 22) {
                         Text("행운" + "을 빌어주는 따봉도치")
                             .font(.Title)
-                            .foregroundStyle(Color.white)
+                            .foregroundStyle(Color.dotchiWhite)
                             
                         HStack {
                             Button(action: {
@@ -40,7 +40,7 @@ struct CollectionView: View {
                             }) {
                                 Text("최신순")
                                     .font(.Head2)
-                                    .foregroundColor(selectedButton == "최신순" ? Color.dotchiGreen : Color.white)
+                                    .foregroundColor(selectedButton == "최신순" ? Color.dotchiGreen : Color.dotchiWhite)
                                     .opacity(selectedButton == "최신순" ? 1.0 : 0.5)
                             }
                             
@@ -49,7 +49,7 @@ struct CollectionView: View {
                             }) {
                                 Text("인기순")
                                     .font(.Head2)
-                                    .foregroundColor(selectedButton == "인기순" ? Color.dotchiGreen : Color.white)
+                                    .foregroundColor(selectedButton == "인기순" ? Color.dotchiGreen : Color.dotchiWhite)
                                     .opacity(selectedButton == "인기순" ? 1.0 : 0.5)
                             }
                         }
@@ -60,21 +60,22 @@ struct CollectionView: View {
                                     ForEach(1...2, id: \.self) { colIndex in
                                         ZStack(alignment: .bottom) {
                                             ZStack(alignment: .top) {
-                                                Image(.imgTest)
+                                                Image(.imgDefaultDummy)
                                                     .resizable()
                                                     .frame(height: 241)
+                                                    .cornerRadius(9.64)
                                                 
-                                                Image(.imgLuckyF)
+                                                Image(.imgLuckyFront)
                                                     .resizable()
                                                     .frame(height: 241)
                                                 
                                                 ZStack {
-                                                    RoundedRectangle(cornerRadius: 60)
-                                                        .fill(Color.green)
+                                                    RoundedRectangle(cornerRadius: 60.25)
+                                                        .fill(Color.dotchiDeepGreen)
                                                         .frame(width: 51, height: 20)
                                                     
                                                     HStack(spacing: 0) {
-                                                        Image(.imgTest)
+                                                        Image(.imgDefaultDummy)
                                                             .resizable()
                                                             .scaledToFill()
                                                             .frame(width: 14, height: 14)
@@ -82,7 +83,7 @@ struct CollectionView: View {
                                                         
                                                         Text("오뜨")
                                                             .font(.S_Sub)
-                                                            .foregroundStyle(Color.white)
+                                                            .foregroundStyle(Color.dotchiWhite)
                                                             .padding(.leading, 4)
                                                     }
                                                     .padding(EdgeInsets(top: 5, leading: 8, bottom: 5, trailing: 12))
@@ -92,7 +93,7 @@ struct CollectionView: View {
                                             
                                             Text("따봉멍멈무")
                                                 .font(.Dotchi_Name2)
-                                                .foregroundStyle(Color.dotchiGreen)
+                                                .foregroundStyle(Color.dotchiDeepGreen)
                                                 .padding(.bottom, 20)
                                         }
                                     }
