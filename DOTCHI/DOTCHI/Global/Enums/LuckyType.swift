fad3c7704649e325c40f88e7cf5d51acb63a5ccb
Jungbin Jung <lollin0@naver.com>
2024-02-16 02:36:52 +0900

    chore: LuckyType에 따른 name, toYouMessage 함수 구현 #33

--- DOTCHI/DOTCHI/Global/Enums/LuckyType.swift
+++ DOTCHI/DOTCHI/Global/Enums/LuckyType.swift
@@ -13,3 +13,32 @@ enum LuckyType: Int {
     case money
     case love
 }
+
+extension LuckyType {
+    
+    func name() -> String {
+        switch self {
+        case .health:
+            return "건강운"
+        case .lucky:
+            return "행운"
+        case .money:
+            return "재물운"
+        case .love:
+            return "애정운"
+        }
+    }
+    
+    func toYouMessage() -> String {
+        switch self {
+        case .health:
+            return "건강운을 나눠 줄게!"
+        case .lucky:
+            return "행운을 나눠 줄게!"
+        case .money:
+            return "재물운을 나눠 줄게!"
+        case .love:
+            return "애정운을 나눠 줄게!"
+        }
+    }
+}
