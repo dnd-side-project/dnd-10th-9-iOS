e8f0ce52e8933a49f22f8a3c4245cdd198b2dc36
Jungbin Jung <lollin0@naver.com>
2024-02-21 18:37:04 +0900

    feat: 따봉도치 만들기 - 미리보기 UI 구현 #32

--- DOTCHI/DOTCHI/Sources/Screens/Make/MakeDotchiPreviewViewController.swift
+++ DOTCHI/DOTCHI/Sources/Screens/Make/MakeDotchiPreviewViewController.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 final class MakeDotchiPreviewViewController: BaseViewController {
     
     private enum Text {
         static let title = "따봉도치 만들기"
         static let upload = "따봉도치 올리기"
+        static let info = "카드를 탭해서 뒤집어 보세요"
     }
     
     // MARK: UIComponents
@@ -23,9 +26,28 @@ final class MakeDotchiPreviewViewController: BaseViewController {
         return view
     }()
     
+    private let cardBackgroundView: UIView = UIView()
+    private let cardFrontView: CardFrontUIView = CardFrontUIView()
+    private let cardBackView: CardBackUIView = CardBackUIView()
+    
+    private let infoLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.setStyle(.sub, .dotchiWhite.withAlphaComponent(0.5))
+        label.text = Text.info
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let uploadButton: DotchiDoneUIButton = {
+        let button: DotchiDoneUIButton = DotchiDoneUIButton()
+        button.setTitle(Text.upload, for: .normal)
+        return button
+    }()
+    
     // MARK: Properties
     
     private var makeDotchiData: MakeDotchiEntity = MakeDotchiEntity()
+    private let disposeBag: DisposeBag = DisposeBag()
     
     // MARK: Initializer
     
@@ -46,21 +68,76 @@ final class MakeDotchiPreviewViewController: BaseViewController {
         
         self.setLayout()
         self.setBackButtonAction(self.navigationView.backButton)
+        self.setTapGesture()
+        self.setData(makeDotchiData: self.makeDotchiData)
     }
     
     // MARK: Methods
     
+    private func setTapGesture() {
+        let tapGesture = UITapGestureRecognizer()
+        tapGesture.rx.event
+            .bind { [weak self] _ in
+                self?.flipCard()
+            }
+            .disposed(by: self.disposeBag)
+        
+        self.cardBackgroundView.addGestureRecognizer(tapGesture)
+    }
     
+    private func flipCard() {
+        let transitionOptions: UIView.AnimationOptions = [.transitionFlipFromRight, .showHideTransitionViews]
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            if !self.cardFrontView.isHidden {
+                UIView.transition(from: self.cardFrontView, to: self.cardBackView, duration: 0.5, options: transitionOptions)
+            } else {
+                UIView.transition(from: self.cardBackView, to: self.cardFrontView, duration: 0.5, options: transitionOptions)
+            }
+        }
+    }
+    
+    private func setData(makeDotchiData: MakeDotchiEntity) {
+        self.cardFrontView.setData(makeDotchiData: makeDotchiData)
+        self.cardBackView.setData(makeDotchiData: makeDotchiData)
+    }
 }
 
 // MARK: - Layout
 
 extension MakeDotchiPreviewViewController {
     private func setLayout() {
-        self.view.addSubviews([navigationView])
+        self.view.addSubviews([navigationView, cardBackgroundView, infoLabel, uploadButton])
+        self.cardBackgroundView.addSubviews([cardBackView, cardFrontView])
         
         self.navigationView.snp.makeConstraints { make in
             make.top.horizontalEdges.equalTo(self.view.safeAreaLayoutGuide)
+        }
+        
+        self.cardBackgroundView.snp.makeConstraints { make in
+            make.top.equalTo(self.navigationView.snp.bottom).offset(60)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(271.adjustedH)
+            make.height.equalTo(400.adjustedH)
+        }
+        
+        self.cardFrontView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        self.cardBackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        self.infoLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.cardBackgroundView.snp.bottom).offset(40)
+            make.horizontalEdges.equalToSuperview().inset(28)
+        }
+        
+        self.uploadButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(28)
+            make.height.equalTo(52)
+            make.bottom.equalToSuperview().inset(32)
         }
     }
 }
