969029257987ad3a6cb0141ec7773b8364987110
Jungbin Jung <lollin0@naver.com>
2024-02-22 03:06:30 +0900

    chore: SignIn Router/Service 코드 구현 #48

--- DOTCHI/DOTCHI/Networks/Routers/AuthRouter.swift
+++ DOTCHI/DOTCHI/Networks/Routers/AuthRouter.swift
@@ -13,56 +13,40 @@ enum SocialType: String {
 }
 
 enum AuthRouter {
-//    case requestSocialLogin(data: SocialLoginRequestDTO)
-//    case requestRefreshToken(data: RefreshTokenRequestDTO)
-//    case requestLogout(data: LogoutRequestDTO)
+    case requestSignIn(data: SignInRequestDTO)
 }
 
-//extension AuthRouter: TargetType {
-//
-//    var baseURL: URL {
-//        return URL(string: APIConstants.baseURL)!
-//    }
-//
-//    var path: String {
-//        switch self {
-//        case .requestSocialLogin:
-//            return "/social/login"
-//        case .requestRefreshToken:
-//            return "/social/refresh"
-//        case .requestLogout:
-//            return "social/logout"
-//        }
-//    }
-//
-//    var method: Moya.Method {
-//        switch self {
-//        case .requestSocialLogin, .requestRefreshToken, .requestLogout:
-//            return .post
-//        }
-//    }
-//
-//    var task: Task {
-//        switch self {
-//        case .requestSocialLogin(let data):
-//            let body: [String: Any] = [
-//                "token": data.token,
-//                "providerType": data.socialType,
-//                "deviceToken": data.deviceToken
-//            ]
-//            return .requestParameters(parameters: body, encoding: JSONEncoding.prettyPrinted)
-//        case .requestRefreshToken(let data):
-//            return .requestJSONEncodable(data)
-//        case .requestLogout(let data):
-//            return .requestJSONEncodable(data)
-//        }
-//    }
-//
-//    var headers: [String: String]? {
-//        switch self {
-//        default:
-//            return ["Content-Type": "application/json"]
-//        }
-//    }
-//}
+extension AuthRouter: TargetType {
 
+    var baseURL: URL {
+        return URL(string: APIConstants.baseURL)!
+    }
+
+    var path: String {
+        switch self {
+        case .requestSignIn:
+            return "/members/login"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .requestSignIn:
+            return .post
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .requestSignIn(let data):
+            return .requestJSONEncodable(data)
+        }
+    }
+
+    var headers: [String: String]? {
+        switch self {
+        default:
+            return ["Content-Type": "application/json"]
+        }
+    }
+}
