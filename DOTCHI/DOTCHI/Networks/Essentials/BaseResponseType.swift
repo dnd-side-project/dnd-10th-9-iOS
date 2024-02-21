ea283012d0985bb3124ad64a34ea1db8761c4489
Jungbin Jung <lollin0@naver.com>
2024-02-22 02:30:17 +0900

    chore: BaseResponseType case 수정 #48

--- DOTCHI/DOTCHI/Networks/Essentials/BaseResponseType.swift
+++ DOTCHI/DOTCHI/Networks/Essentials/BaseResponseType.swift
@@ -14,14 +14,14 @@ struct BaseResponseType<T: Decodable>: Decodable {
     var result: T?
     
     enum CodingKeys: String, CodingKey {
-        case status = "code"
+        case code = "code"
         case message = "message"
         case result = "result"
     }
     
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
-        code = (try? values.decode(Int.self, forKey: .status)) ?? -1
+        code = (try? values.decode(Int.self, forKey: .code)) ?? -1
         message = (try? values.decode(String.self, forKey: .message)) ?? ""
         result = (try? values.decode(T.self, forKey: .result)) ?? nil
     }
