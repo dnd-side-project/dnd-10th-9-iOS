f5f6d12f4168641d3a75ee5ac813edd79d5ccd2b
Jungbin Jung <lollin0@naver.com>
2024-02-22 01:53:48 +0900

    add: SignInViewController 생성 #48

---
+++ DOTCHI/DOTCHI/Sources/Screens/SignIn/SignInViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignInViewController.swift
+//  DOTCHI
+//
+//  Created by Jungbin on 2/22/24.
+//
+
+import UIKit
+import SnapKit
+
+final class SignInViewController: BaseViewController {
+    
+    // MARK: UIComponents
+    
+    
+    
+    // MARK: Properties
+    
+    
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setLayout()
+    }
+    
+    // MARK: Methods
+    
+    
+}
+
+// MARK: - Layout
+
+extension SignInViewController {
+    private func setLayout() {
+        self.view.addSubviews([])
+    }
+}
