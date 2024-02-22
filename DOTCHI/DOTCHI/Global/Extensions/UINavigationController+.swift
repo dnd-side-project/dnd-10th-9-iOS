2481940b269053b83a71080f07c1dec26dcb7dd3
Jungbin Jung <lollin0@naver.com>
2024-02-23 02:37:52 +0900

    chore: UINavigationController, BaseUINavigationController 기능 분리 #55
    SwiftUI에서도 스와이프하여 뒤로가기 사용하기 위함

---
+++ DOTCHI/DOTCHI/Global/Extensions/UINavigationController+.swift
@@ -0,0 +1,19 @@
+//
+//  UINavigationController+.swift
+//  DOTCHI
+//
+//  Created by Jungbin on 2/23/24.
+//
+
+import UIKit
+
+extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
