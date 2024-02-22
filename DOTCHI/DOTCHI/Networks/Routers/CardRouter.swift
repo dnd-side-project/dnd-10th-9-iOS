3f9da43cce787f2f971f371e3dc199d990294244
Jungbin Jung <lollin0@naver.com>
2024-02-22 15:31:01 +0900

    chore: postCard Router, Service 코드 작성 #52

---
+++ DOTCHI/DOTCHI/Networks/Routers/CardRouter.swift
@@ -0,0 +1,57 @@
+//
+//  CardRouter.swift
+//  DOTCHI
+//
+//  Created by Jungbin on 2/22/24.
+//
+
+import Foundation
+import Moya
+
+enum CardRouter {
+    case postCard(data: PostCardRequestDTO)
+}
+
+extension CardRouter: TargetType {
+
+    var baseURL: URL {
+        return URL(string: APIConstants.baseURL)!
+    }
+
+    var path: String {
+        switch self {
+        case .postCard:
+            return "/card"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .postCard:
+            return .post
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .postCard(let data):
+            return .uploadMultipart(data.toMultipartFormData())
+        }
+    }
+
+    var headers: [String: String]? {
+        switch self {
+        case .postCard:
+            return [
+                "Content-Type": "multipart/form-data",
+                "Authorization": UserInfo.shared.accessToken,
+            ]
+//        default:
+//            return [
+//                "Content-Type": "application/json",
+//                "Authorization": UserInfo.shared.accessToken,
+//            ]
+        }
+    }
+}
+
