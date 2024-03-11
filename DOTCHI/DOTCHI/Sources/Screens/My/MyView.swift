fd208784e922b74da2ef1539022264930e09cb7f
YuBin Kim <yubin412@naver.com>
2024-03-11 19:26:55 +0900

    chore: 마이페이지 / card dotchi image layout 수정 #92

--- DOTCHI/DOTCHI/Sources/Screens/My/MyView.swift
+++ DOTCHI/DOTCHI/Sources/Screens/My/MyView.swift
@@ -167,11 +167,15 @@ struct MyCardView: View {
             isDetailPresented = true
         }) {
             ZStack(alignment: .bottom) {
-                ZStack(alignment: .top) {
-                    AsyncImageView(url: URL(string: card.cardImageUrl ?? ""))
-                        .scaledToFill()
-                        .frame(width: 163, height: 241)
-                        .cornerRadius(9.64)
+                ZStack(alignment: .center) {
+                    GeometryReader { geometry in
+                        AsyncImageView(url: URL(string: card.cardImageUrl ?? ""))
+                            .scaledToFill()
+                            .frame(width: geometry.size.width * (210.0 / 270.0),
+                                   height: geometry.size.width * (210.0 / 270.0))
+                            .position(x: geometry.size.width * 0.5,
+                                      y: geometry.size.height * 0.46)
+                    }
                     
                     Image(getFrontImageName(forThemeId: card.themeId))
                         .resizable()
